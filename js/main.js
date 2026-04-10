fbd4830d4f9f08a8a3ed563113bb3bdf7d5848d9
0gemy <gemy1gg@gmail.com>
2026-04-10 10:52:00 +0200

    Refactor main.js to improve structure and add preloader

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+
+window.addEventListener("load", function () {
+  const preloader = document.getElementById("preloader");
+  preloader.style.opacity = "0";
+  preloader.style.visibility = "hidden";
+  preloader.style.transition = "0.5s";
+});
 /* ============================================================
    Navbar
    ============================================================ */
